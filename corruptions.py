cebbac947d294ed2a5cc942f1e1fb71523cb9043
Alex Zhao <axyzhao@berkeley.edu>
2020-10-23 16:03:26 -0700

    add corruptions file

---
+++ corruptions.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def gaussian_noise(x, severity=1):
+    c = [.08, .12, 0.18, 0.26, 0.38][severity - 1]
+
+    x = np.array(x) / 255.
+    return np.clip(x + np.random.normal(size=x.shape, scale=c), 0, 1) * 255
